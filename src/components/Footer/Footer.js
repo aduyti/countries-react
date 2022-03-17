26779116c2529a48910494e38fcc47f9922982f5
Anirudhya <anirudhyaduyti@gmail.com>
2022-03-17 23:46:55 +0600

    boot

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="bg-primary">
+            <p className="fst-italic py-3 mb-0 text-danger">Copyright &copy; Protected</p>
+        </div>
+    );
+};
+
+export default Footer;
